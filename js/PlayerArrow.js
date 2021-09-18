5c34f8e4f6bdff3672dd86acc41d85a6c8ada077
bhavyajhabak <84122871+bhavyajhabak@users.noreply.github.com>
2021-09-18 23:16:58 +0530

    Add files via upload

--- js/PlayerArrow.js
+++ js/PlayerArrow.js
@@ -16,7 +16,7 @@ class PlayerArrow {
     
     shoot(archerAngle) {
       var velocity = p5.Vector.fromAngle(archerAngle);
-      velocity.mult(20);
+      velocity.mult(50);
       Matter.Body.setStatic(this.body, false);
       Matter.Body.setVelocity(this.body, { x: velocity.x, y: velocity.y });
     }
